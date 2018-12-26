99e14452838eb61636cf78138a7bc9e73b80d506
CoheeStudio <alwayssmile11a1@gmail.com>
2018-12-26 17:26:47 +0700

    EditorApp

---
+++ Source/Core/Cohee/CoheeApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cohee
+{
+    public static class CoheeApp
+    {
+        public const string DataDirectory = "Data";
+        public const string PluginDirectory = "Plugins";
+        public const string CmdArgDebug = "debug";
+        public const string CmdArgEditor = "editor";
+        public const string CmdArgProfiling = "profile";
+    }
+}
--- Source/Editor/CoheeEditor/CoheeEditorApp.cs
+++ Source/Editor/CoheeEditor/CoheeEditorApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,78 @@ namespace Cohee.Editor
         public const string EditorPrevLogfileDir = "Temp";
 
 
+        private static bool needsRecovery = false;
+        private static MainWindow mainWindow = null;
+        private static EditorLogOutput memoryLogOutput = null;
+        private static bool appSuspended = true;
 
         public static void Init(MainWindow mainWindow)
         {
+            CoheeEditorApp.mainWindow = mainWindow;
 
+            // Set up an in-memory data log so plugins can access the log history when needed
+            CoheeEditorApp.memoryLogOutput = new EditorLogOutput();
+            Logs.AddGlobalOutput(memoryLogOutput);
+
+            // Create working directories, if not existing yet.
+            if (!Directory.Exists(CoheeApp.DataDirectory))
+            {
+                Directory.CreateDirectory(CoheeApp.DataDirectory);
+                using (FileStream s = File.OpenWrite(Path.Combine(CoheeApp.DataDirectory, "WorkingFolderIcon.ico")))
+                {
+                    Properties.GeneralRes.IconWorkingFolder.Save(s);
+                }
+                using (StreamWriter w = new StreamWriter(Path.Combine(CoheeApp.DataDirectory, "desktop.ini")))
+                {
+                    w.WriteLine("[.ShellClassInfo]");
+                    w.WriteLine("ConfirmFileOp=0");
+                    w.WriteLine("NoSharing=0");
+                    w.WriteLine("IconFile=WorkingFolderIcon.ico");
+                    w.WriteLine("IconIndex=0");
+                    w.WriteLine("InfoTip=This is Dualitors working folder");
+                }
+
+                DirectoryInfo dirInfo = new DirectoryInfo(CoheeApp.DataDirectory);
+                dirInfo.Attributes |= FileAttributes.System;
+
+                FileInfo fileInfoDesktop = new FileInfo(Path.Combine(CoheeApp.DataDirectory, "desktop.ini"));
+                fileInfoDesktop.Attributes |= FileAttributes.Hidden;
+
+                FileInfo fileInfoIcon = new FileInfo(Path.Combine(CoheeApp.DataDirectory, "WorkingFolderIcon.ico"));
+                fileInfoIcon.Attributes |= FileAttributes.Hidden;
+            }
+            if (!Directory.Exists(CoheeApp.PluginDirectory)) Directory.CreateDirectory(CoheeApp.PluginDirectory);
+            if (!Directory.Exists(EditorHelper.SourceDirectory)) Directory.CreateDirectory(EditorHelper.SourceDirectory);
+            if (!Directory.Exists(EditorHelper.SourceMediaDirectory)) Directory.CreateDirectory(EditorHelper.SourceMediaDirectory);
+            if (!Directory.Exists(EditorHelper.SourceCodeDirectory)) Directory.CreateDirectory(EditorHelper.SourceCodeDirectory);
+
+            // Initialize Package Management system
+            packageManager = new PackageManager();
+
+            // Initialize Duality
+            EditorHintImageAttribute.ImageResolvers += EditorHintImageResolver;
+            DualityApp.PluginManager.PluginsReady += DualityApp_PluginsReady;
+            DualityApp.Init(
+                DualityApp.ExecutionEnvironment.Editor,
+                DualityApp.ExecutionContext.Editor,
+                new DefaultAssemblyLoader(),
+                null);
+
+            // Initialize the plugin manager for the editor. We'll use the same loader as the core.
+            pluginManager.Init(DualityApp.PluginManager.AssemblyLoader);
+
+            // Need to load editor plugins before initializing the graphics context, so the backend is available
+            pluginManager.LoadPlugins();
+
+            // Need to initialize graphics context and default content before instantiating anything that could require any of them
+            InitMainGraphicsContext();
+            DualityApp.InitPostWindow();
+
+            LoadUserData();
+            pluginManager.InitPlugins();
+
+            // Allow the engine to run
+            appSuspended = false;
         }
 
     }
